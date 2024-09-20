8c4b3248a510cbb234576771108355f75f7314dd
root <root@Iron-Patriot>
2024-09-20 17:00:58 +0100

    Wuodan First Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wuodan",
+    version="0.1.0",
+    packages=['wuodan'],  
+    include_package_data=True,
+    install_requires=[
+        "tqdm",
+        "psutil"
+    ],
+    entry_points={
+        "console_scripts": [
+            "wuodan=wuodan.wuodan:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)

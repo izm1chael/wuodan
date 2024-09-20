51afb505a05de0c7076453b04808c96dae2fc652
Mike Johnson <m1chael.johnson1804@protonmail.com>
2024-09-20 17:33:51 +0100

    Update setup.py
    Bump version number for PyPi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="wuodan",
-    version="0.1.0",
+    version="0.1.1",
     packages=['wuodan'],  
     include_package_data=True,
     install_requires=[

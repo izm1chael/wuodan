24af43e97673d595cda3f5bc81a77825a7d1bad6
Mike Johnson <m1chael.johnson1804@protonmail.com>
2024-09-20 17:23:43 +0100

    Update setup.py
    Add long_description  markdown

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="wuodan",
     version="0.1.0",
